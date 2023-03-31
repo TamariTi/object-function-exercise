ede7645a04a2b86bd284c427fcb1e8adef3fbeb7
Tavion Boc <tavionboc@Tavions-MBP.lan>
2023-03-30 19:31:33 -0700

    done

--- challenges.js
+++ challenges.js
@@ -1,21 +1,46 @@
 // Create a variable called myName and assign it your name as a string.
-
+let myName = 'tamari'
 // Create an array called numbers and add five numbers to it.
-
+let numbers = [2,6,9,8,10]
 // Create a variable called myAge and assign it your age as a number.
-
+let myAge = 23
 // Create an object called person with properties name, age, and gender.
-
+let person = {
+    name: 'tamari',
+    age: 23,
+    gender: 'male'
+}
 // Create a variable called isRaining and assign it a boolean value based on whether it is currently raining or not.
-
+function whether(value) {
+    let isRaining 
+    if(value == 'rain') {
+        isRaining = false
+        return isRaining
+    } else {
+        isRaining = false
+        return isRaining
+    }
+}
 // Create a function called greet that takes a name as a parameter and returns a greeting message using template literals.
-
+function greet(name) {
+    return `hello ${name}`
+}
 // Create a function called multiply that takes two numbers as parameters and returns their product.
-
+function multiply(num1,num2) {
+    return num1 * num2
+}
 // Create a function called divide that takes two numbers as parameters and returns their quotient.
-
+function divide(num1,num2) {
+    return num1 / num2
+}
 // BONUS
 // Create a function called isEven that takes a number as a parameter and returns true if the number is even, false otherwise.
 
-
+function isEven(num) {
+    if(num % 2 == 0){
+        return true
+    } else {
+        return false
+    }
+}
 
